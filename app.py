30dfb737d45625412c9b802df1157e38148f03e7
meryfrancy07-ai <meryfrancy07@gmail.com>
2026-07-27 12:50:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -195,6 +195,9 @@ with tab2:
     🎭 **Esibizionismo Pubblico:**
     Le finte proposte e le finte litigate sono valide ai fini del punteggio SOLO se riescono ad attirare l'attenzione di almeno 5 passanti.
     """)
+
+    🍹 **Regola sui drink:**
+    Il bonus inerente ai dirnk bevuti verra contato solo dal secodno drik in poi (secodno escluso) per le donne, dal terzo in poi (terzo escluso) per gli uomini.
     
     st.error("""
     🚨 **! AVVISO DI INFAMIA !** 🚨  
